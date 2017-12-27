d08065551f1fc65baa582b2111ae552891981825
Piotr Bakalarski <piotrb5e3@gmail.com>
2017-12-27 21:37:00 +0100

    Add classifiers and short package readme

--- at89overlord/setup.py
+++ at89overlord/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='at89overlord',
-    version='0.2.1',
+    version='0.2.2',
     description='Arduino-based AT89C2051 programmer',
     url='https://github.com/piotrb5e3/AT89C2051_programmer',
     author='Piotr Bakalarski',
     author_email='piotrb5e3@gmail.com',
-    license='GNU General Public License v3.0',
+    license='GNU General Public License v3 (GPLv3)',
     install_requires=[
         'intelhex',
         'pyserial',
@@ -20,4 +20,21 @@ setup(
             'at89overlord = at89overlord.at89runner:main',
         ],
     },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: Embedded Systems',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Console',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
