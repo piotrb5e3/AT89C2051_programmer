03c3a7e49cb077bc523dd1092d7a772081f35fe6
Piotr Bakalarski <piotrb5e3@gmail.com>
2018-01-06 16:18:59 +0100

    Version bump

--- at89overlord/setup.py
+++ at89overlord/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='at89overlord',
-    version='0.2.2',
+    version='0.3.0',
     description='Arduino-based AT89C2051 programmer',
     url='https://github.com/piotrb5e3/AT89C2051_programmer',
     author='Piotr Bakalarski',
